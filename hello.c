0ef373544442dd86bee9094fad9e929243c9476b
Noah Corey <ncorey2@huskers.unl.edu>
2023-08-24 15:31:50 -0500

    Added a description to my code

--- hello.c
+++ hello.c
@@ -3,14 +3,14 @@
  * Date: 2023/08/24
  *
  *
- *
+ *  A simple code in C that states my name and college major
 */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Noah Corey\n");
+  printf("Noah Corey!\n");
   printf("My major is Computer Science!\n");
 
 return 0;
