df4aea0956fc6854bad64e665d662d5287b39648
Noah Corey <ncorey2@huskers.unl.edu>
2023-08-24 11:47:32 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,16 @@
-Noah Corey
+/**
+ * Author: Noah Corey
+ * Date: 2023/08/24
+ *
+ * 
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Major: Computer Science!\n");
+return 0;
+}
