481fd6428c44f383fa9f801cb60d48d74d5b6575
JamesHawkins1 <jhawkins1@huskers.unl.edu>
2023-08-25 17:14:19 -0500

    printed name and major

---
+++ hellonoah.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Noah Corey
+ * Date: 2023/08/24
+ *
+ *
+ *  A simple code in C that states my name and college major
+ * @author ncorey2
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Noah Corey!\n");
+  printf("My major is Computer Science!\n");
+  printf("James Hawkins\n");
+  printf("Computer Sceicne\n");
+
+return 0;
+}
